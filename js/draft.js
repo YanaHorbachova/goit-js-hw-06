6443545f3bf131d2a393b1859615af5123b840aa
YanaHorbachova <maya69877@gmail.com>
2021-01-24 21:01:17 +0200

    сдлано задание 9

--- js/draft.js
+++ js/draft.js
@@ -66,6 +66,19 @@ const calculateTotalBalance = users => users.reduce((acc,user) => acc + user.bal
 console.log(calculateTotalBalance(users)); // 20916
 
 
+// Задание 9
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => 
+users
+    .sort(
+      (prevUser, nextUser) => prevUser.friends.length - nextUser.friends.length)
+    .map((user) => user.name);
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+
 // Задание 10
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
--- js/task.js
+++ js/task.js
@@ -11,13 +11,4 @@ const getUsersWithFriend = (users, friendName) => {
 console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-// Задание 9
-// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
-
-const getNamesSortedByFriendsCount = users => {
-
-};
-
-console.log(getNamesSortedByFriendsCount(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
