099e8e4da4eb49f106da69a941f44845351328c1
YanaHorbachova <maya69877@gmail.com>
2021-01-24 12:43:29 +0200

    Задание 1

--- js/task.js
+++ js/task.js
@@ -1,9 +1,9 @@
+import users from "./users.js";
+
 // Задание 1
 // Получить массив имен всех пользователей (поле name).
 
-const getUserNames = users => {
-
-};
+const getUserNames = users => users.map((user) => user.name);
 
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
