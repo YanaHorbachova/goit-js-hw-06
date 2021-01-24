1aa461611a895c047a4081df11f98e1f793c6725
YanaHorbachova <maya69877@gmail.com>
2021-01-24 19:39:10 +0200

    сделано задание 6

--- js/draft.js
+++ js/draft.js
@@ -30,3 +30,14 @@ const getUserWithEmail = (users, email) => users.find ((user) => user.email === 
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+// Задание 6
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => users.filter((user) =>  user.age >= min && user.age <= max);
+
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
--- js/task.js
+++ js/task.js
@@ -17,15 +17,7 @@ console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazq
 
 
 
-// Задание 6
-// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
 
-const getUsersWithAge = (users, min, max) => users.filter((user) =>  min > user.age < max);
-
-console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-
-console.log(getUsersWithAge(users, 30, 40));
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
 // Задание 7
 // Получить общую сумму баланса (поле balance) всех пользователей.
